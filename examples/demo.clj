6097f958d429f1afaca2a17535dbcb59f1bdf3f1
Ragnar Dahlén <r.dahlen@gmail.com>
2009-06-05 18:05:22 +0200

    Some directory layout restructuring

---
+++ examples/demo.clj
@@ -0,0 +1,19 @@
+(ns demo
+  (:require redis))
+
+
+(redis/with-server
+  {:host "127.0.0.1" :port 6379 :db 0}
+  (do
+    (println "Sending ping")
+    (println "Reply:" (redis/ping))
+    (println "Server info:")
+    (let [info (redis/info)]
+      (dorun
+       (map (fn [entry]
+              (println (str "- "(first entry) ": " (last entry)))) info)))
+    (println "Setting key 'foo' to 'bar'")
+    (println "Reply:" (redis/set "foo" "bar"))
+    (println "Getting value of key 'foo'")
+    (println "Reply:" (redis/get "foo"))))
+
