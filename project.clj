ed25da8c48f640a29a17798efef33b4471cb28b4
Ragnar Dahlén <r.dahlen@gmail.com>
2010-06-10 07:40:30 +0200

    Update dev-dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject redis-clojure "1.0-SNAPSHOT"
+(defproject redis-clojure "1.0.3-SNAPSHOT"
   :description "A library for the Redis protocol"
   :url "http://github.com/ragnard/redis-clojure"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[org.clojure/swank-clojure "1.0"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [lein-clojars "0.5.0"]])
