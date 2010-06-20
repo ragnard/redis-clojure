deb91b98036c86f049c50f2f8a70115795275334
Ragnar Dahlén <r.dahlen@gmail.com>
2010-06-20 15:57:14 +0200

    Remove/cleanup old stuff

--- benchmarks/ruby.clj
+++ benchmarks/ruby.clj
@@ -1,25 +1,61 @@
+;
+; A few simple benchmarks, inspired by redis-rb.
+;
 (ns benchmarks.ruby
   (:require redis))
 
-(dotimes [n 4]
+(set! *warn-on-reflection* true)
+
+(defmacro measure-time [body]
+  "Call fn and return the time it took in seconds."
+  `(let [start# (. System (nanoTime))]
+    ~body
+    (/ (- (. System (nanoTime)) start#) 1000000000.0)))
+
+
+(defn ping [n]
+  (print "Performing" n "PING commands: ")
+  (measure-time
+   (dotimes [i n]
+     (redis/ping))))
+
+(defn set [n]
+  (print "Setting" n "keys: ")
+  (measure-time
+   (dotimes [i n]
+     (let [key (str "foo" i)])
+     (redis/set key "xxx"))))
+
+(defn set-and-get [n]
+  (print "Setting and getting" n "keys: ")
+  (measure-time
+   (dotimes [i n]
+     (let [key (str "foo" i)]
+       (redis/set key (apply str (repeat 10 key)))
+       (redis/get key)))))
+
+(defn set-and-get-pipelined [n]
+  (print "Setting and getting" n "keys (pipelined): ")
+  (measure-time
+   (dotimes [i n]
+     (let [key (str "foo" i)]
+       (redis/pipeline 
+        (redis/set key (apply str (repeat 10 key)))
+        (redis/get key))))))
+
+(defn run-benchmark [fn iterations]
   (redis/with-server 
-   {:db 15}
-   (redis/set "foo" "The first line we sent to the server is some text")
-   (time 
-    (dotimes [i 20000]
-      (let [key (str "key" i)]
-        (redis/set key "The first line we sent to the server is some text")
-        (redis/get "foo"))))))
+    {:db 15}
+    (let [elapsed (apply fn iterations nil)
+          kops (* 2 (/ iterations 1000 elapsed))]
+      (println (format "%.2f" kops) "KOp/s"))))
 
+(dotimes [i 4]
+  (run-benchmark ping 20000)
+  (run-benchmark set 20000)
+  (run-benchmark set-and-get 20000)
+  (run-benchmark set-and-get-pipelined 20000))
 
-;(redis/with-server 
-; {}
-; (redis/set "foo" "The first line we sent to the server is some text")
-; (time 
-;  (dotimes [i 20000]
-;    (let [key (str "push_trim" i)]
-;      (redis/lpush key i)
-;      (redis/ltrim key 0 30)))))
 
 
 
--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject redis-clojure "1.0.3-SNAPSHOT"
-  :description "A library for the Redis protocol"
-  :url "http://github.com/ragnard/redis-clojure"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
-                     [lein-clojars "0.5.0"]])
