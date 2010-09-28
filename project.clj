29ee928029ba9dad19f5fc59293db1ae34c51866
Ragnar Dahlén <r.dahlen@gmail.com>
2010-09-28 10:56:49 +0200

    Fix project.clj, ignore pom.xml

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject leiningen "redis-clojure"
+(defproject redis-clojure "1.2.0"
   :description "A client library for Redis"
   :url "http://github.com/ragnard/redis-clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
