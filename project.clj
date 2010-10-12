8cf47e309b8a39da3b0cd0ab711e3c5d3f976088
Aaron Bedra <aaron@aaronbedra.com>
2010-10-12 07:55:45 -0700

    updating dependencies in project.clj to 1.2

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject redis-clojure "1.0.4"
   :description "A library for the Redis protocol"
   :url "http://github.com/ragnard/redis-clojure"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
